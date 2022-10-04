1a3f3d6c36363f4161b851381aa0684f006d5001
unknown <mobilelegendsazza@gmail.com>
2022-10-04 10:42:42 +0700

    test projek 2

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+int main()
+{
+const float pi = 3.14;
+int jari;
+float luas;
+printf("masukan nilai jari-jari : %i",jari);
+scanf("%i", &jari);
+
+luas = (jari*jari) * pi;
+printf("luas lingkaran : %f cm^2",luas);
+
+    return 0;
+
+
+}
